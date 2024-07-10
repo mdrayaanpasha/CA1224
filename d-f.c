2b584fe2db9d0b9cce2b8cf955d8a714cda873a9
Mohammed Rayaan Pasha <134199206+mdrayaanpasha@users.noreply.github.com>
2024-07-10 21:14:41 +0530

    Create d-f.c

---
+++ d-f.c
@@ -0,0 +1,24 @@
+/* -----------------------------
+d-f.C
+rayaan
+10/07/24
+----------------------------- */
+
+#include <stdio.h>
+
+int main() {
+    printf("the following is a programme to convert degrees to fahrenheit \n");
+    float c,f;
+    
+    printf("Insert temparature in fahrenheit: \n");
+    scanf("%f",&c);
+    f=(9.0/5.0) * c +32;
+    
+    printf("%f",f);
+    
+    
+    
+
+    
+    return 0;
+}
