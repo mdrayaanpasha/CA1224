c8b8ad7fd0a71e96eb927159f477deae503fb441
Mohammed Rayaan Pasha <134199206+mdrayaanpasha@users.noreply.github.com>
2024-07-10 22:28:15 +0530

    Update d-f.c

--- d-f.c
+++ d-f.c
@@ -10,7 +10,7 @@ int main() {
     printf("the following is a programme to convert degrees to fahrenheit \n");
     float c,f;
     
-    printf("Insert temparature in fahrenheit: \n");
+    printf("Insert temparature in Celcius: \n");
     scanf("%f",&c);
     f=(9.0/5.0) * c +32;
     
