5450b4f1d8d78d1857ad7b193529fd8b0a9099ae
Mohammed Rayaan Pasha <134199206+mdrayaanpasha@users.noreply.github.com>
2024-07-27 21:52:33 +0530

    Update matrixInpOut.c

--- matrixInpOut.c
+++ matrixInpOut.c
@@ -6,14 +6,15 @@
 #include <stdio.h>
 
 int main() {
-    int m,n,arr[m][n],i,j;
+    int m,n,i,j;
     
     printf("This is a programme to input and output in a matrix!\n How many rows and cols do you want? \n");
     scanf("%d%d",&m,&n);
     
     
     printf("Enter Data in your matrix\n");
-    
+    int arr[m][n];
+  
     for(i=0;i<m;i++){
         for(j=0;j<n;j++){
             printf("Enter Data at: %d %d: ",i,j);
