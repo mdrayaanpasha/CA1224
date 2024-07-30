2b9a01eb571847b88894f1e8bfb73075a182d218
Mohammed Rayaan Pasha <134199206+mdrayaanpasha@users.noreply.github.com>
2024-07-30 10:44:27 +0530

    Update PorF.c

--- PorF.c
+++ PorF.c
@@ -9,22 +9,23 @@ __________________________________________________________*/
 
 int main(){
     int m1,m2,m3,avg,sum;
-    char name[10];
+    char name[10],RegNo[10];
     
-    printf("Whats your name? \n");
-    scanf("%s",&name);
+    printf("Whats your name & Reg no? \n");
+    scanf("%s%s",&name,&RegNo);
     
-    printf("Enter Your marks in 3 subjects: \n");
+    printf("Enter Marks in 3 subjects: \n");
     scanf("%d%d%d",&m1,&m2,&m3);
     
     sum=m1+m2+m3;
     
     avg= sum/3;
-    
+    printf("------------------------------------------------\nSt.Joseph's University\n Marks Card");
     if(avg < 40){
-        printf("%s you got %d that implies your failed!\n",name,sum);
+        printf("Regno: %s \n Name: %s\n Physics: %d \n Math: %d \n Computer Science: %d \n Total: %d \n Result: Failed!",RegNo,name,m1,m2,m3,sum);
     }else{
-        printf("%s you got %d that implies your passed!\n",name,sum);
+         printf("\n Regno: %s \n Name: %s\n Physics: %d \n Math: %d \n Computer Science: %d \n Total: %d \n Result: PASSED! ",RegNo,name,m1,m2,m3,sum);
     }
+    printf(" \n-----------------------------------------------");
     return 0;
 }
