a19bdfa0175d4cfbc236c13d46acfaf28a8e74d3
Mohammed Rayaan Pasha <134199206+mdrayaanpasha@users.noreply.github.com>
2024-08-20 11:02:39 +0530

    Create SumOddSeries.c

---
+++ SumOddSeries.c
@@ -0,0 +1,31 @@
+/*-------------------------------------
+            Rayaan Pasha
+            SumOddSeries.c
+            20-08-24
+--------------------------------------
+*/
+#include <stdio.h>
+
+int main() {
+    int N,Sum,Term,i;
+    
+    printf("This Programme is to compute the sum of n series!\n Whats N?\n");
+    scanf("%d",&N);
+    i=Term=1;
+    Sum=0;
+    
+    while(i<=N){
+        Sum+=Term;
+        if(i!=N){
+            printf("%d + ",Term); 
+        }else{
+            printf("%d ",Term);
+        }
+        
+        i+=2;
+        Term+=2;
+        
+    }
+    printf("\nThe Sum is: %d",Sum);
+    return 0;
+}
