dc490b755237b969df2d0866cc073b637299e134
Mohammed Rayaan Pasha <134199206+mdrayaanpasha@users.noreply.github.com>
2024-09-10 10:47:35 +0530

    Create AreaRect.c

---
+++ AreaRect.c
@@ -0,0 +1,16 @@
+/*=================================================
+                    AreaRect.c
+                    Rayaan Pasha
+                    10-09-24
+==================================================*/
+#include <stdio.h>
+
+int AreaRect(int l,int b){
+    
+    return l*b;
+}
+
+int main() {
+    printf("%d",AreaRect(1,2));
+    return 0;
+}
